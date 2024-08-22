c77b9c2e735de866c2a75f08a8c19db1f4eb4a4d
daniegitx1 <mailbox.2332@proton.me>
2024-08-22 17:10:00 +0200

    third commit

--- sats/views.py
+++ sats/views.py
@@ -3,12 +3,13 @@ import requests
 from decimal import Decimal
 from django.shortcuts import render
 
+
 def index(request):
     # Your API URL for Speed balance
     url = "https://api.tryspeed.com/balances"
     headers = {
         "accept": "application/json",
-        "authorization": "Basic c2tfbGl2ZV9seWs0bzY5cDd2Ym5UWlU0bHpjaHIwYzM1ajIwUTRLTWx6Y2hyMGMzYzQzV0t5enI6VGhlVmlsbGFnZUh1YjE="
+        "authorization": "Basic cmtfbGl2ZV9seWs0bzY5cDd2Ym5UWlU0bHplMGs0bHdGUDV0NVEzcmx6ZTBrNGx3eXpNelprQVQ6VGhlVmlsbGFnZUh1YjE="
     }
     response = requests.get(url, headers=headers)
     response_data = response.json()
@@ -17,13 +18,25 @@ def index(request):
     # Convert to BTC
     btc_amount = Decimal(speed_balance) / Decimal(100000000)
 
+    # Get form data satsinput
+    if request.method == 'POST':
+        satsinput = request.POST.get('satsinput')
+        btc_amount2 = float(satsinput)
+        btc_amount2 = Decimal(btc_amount2) / Decimal(100000000)
+        btc_amount3 = int(btc_amount2*100000000)
+    else:
+        btc_amount2 = 0
+        btc_amount3 = 0
+
     # Get ZAR price
     zar_price = get_bitcoin_price()
 
     # Calculate ZAR value
     zar_value = btc_amount * zar_price
+    zar_value2 = btc_amount2 * zar_price
+    zar_value3 = zar_price
 
-    context = {'zar_value': zar_value, 'speed_balance': int(speed_balance),}
+    context = {'zar_value': zar_value, 'zar_value2': zar_value2, 'zar_value3': zar_value3, 'btc_amount3': btc_amount3, 'speed_balance': int(speed_balance),}
     return render(request, 'sats/index.html', context)
 
 def get_bitcoin_price():
@@ -31,4 +44,6 @@ def get_bitcoin_price():
     response = requests.get(url)
     data = response.json()
     return Decimal(data.get("bitcoin", {}).get("zar", 0))
+
+
 
--- satsProject/settings.py
+++ satsProject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sats.apps.SatsConfig',
+    'django.contrib.humanize'
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+USE_THOUSAND_SEPARATOR = 'True'
